5a8c9878f136285e7bb7e82138894482e3a524d0
彩虹工作室 <rain.wen@outlook.com>
2022-01-26 00:39:14 +0800

    。

--- build/html/_sources/index.rst.txt
+++ build/html/_sources/index.rst.txt
@@ -1,17 +1,18 @@
-.. 彩虹工作室项目文档, created by
-   sphinx-quickstart on Fri Dec 31 20:58:41 2021.
+.. 彩虹工作室文档 documentation master file, created by
+   sphinx-quickstart on Wed Jan 26 00:15:24 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-首页
-==============================================
+彩虹工作室文档
+==========================================
 
 .. toctree::
    :maxdepth: 2
+   :caption: 项目:
 
-   企业微信/index
-   助手/index
-   about
+   wecom/index
+   assistant/index
+   theme/index
 
 Indices and tables
 ==================
--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,6 @@
+import sphinx_theme
+from recommonmark.parser import CommonMarkParser
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -17,9 +20,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "彩虹工作室项目文档"
-copyright = "2021, 彩虹工作室"
-author = "rain.wen@outlook.com"
+project = "彩虹工作室文档"
+copyright = "2022, 彩虹工作室"
+author = "彩虹工作室"
 
 # The full version, including alpha/beta/rc tags
 release = "14.0"
@@ -30,7 +33,7 @@ release = "14.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["recommonmark"]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -53,13 +56,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster' sphinx_rtd_theme classic
-html_theme = "sphinx_rtd_theme"
+# html_theme = 'alabaster'
+html_theme = "stanford_theme"
+html_theme_path = [sphinx_theme.get_html_theme_path("stanford-theme")]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
 
 from recommonmark.parser import CommonMarkParser
 
@@ -67,4 +72,3 @@ source_parsers = {
     ".md": CommonMarkParser,
 }
 source_suffix = [".rst", ".md"]
-
